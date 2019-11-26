41dffaab351146b2850d257108464518b51d1263
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2019-11-26 21:43:45 +0300

    Add correct fronted

--- app_craigs/urls.py
+++ app_craigs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, new_search_view
 
 urlpatterns = [
 	path('', home_view, name = 'home_view'),
+	path('new_search', new_search_view, name = 'new_search_view' )
 ]
--- app_craigs/views.py
+++ app_craigs/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
 	return render(request, 'base.html', {})
+
+def new_search_view(request):
+	searched_item = request.POST.get('search')
+	context_for_frontend = {
+		'searched_item': searched_item
+	}
+	return render(request, 'app_craigs/new_search.html', context_for_frontend)
