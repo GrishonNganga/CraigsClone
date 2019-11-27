369b7b510a195c2194c03a9c688bfa8e76615a41
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2019-11-27 13:28:40 +0300

    Bug fixing

--- craigsApp/settings.py
+++ craigsApp/settings.py
@@ -128,6 +128,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =  (os.path.join(BASE_DIR, 'static'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
