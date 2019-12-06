028cc0bacc168b202e73f76df0202f5eb7615b19
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2019-12-06 19:56:09 +0300

    Adding Jumia Black Friday Code

--- app_craigs/views.py
+++ app_craigs/views.py
@@ -4,7 +4,7 @@ import requests
 from requests.compat import quote_plus
 from .models import Search
 
-BASE_CRAIGS_URL = 'https://kenya.craigslist.org/search/sss?query={}'
+BASE_JUMIA_URL = 'https://www.jumia.co.ke/catalog/?q={}'
 BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 # Create your views here.
@@ -20,20 +20,21 @@ def new_search_view(request):
 	data = response.text
 	soup = BeautifulSoup(data, features = 'html.parser')
 	
-	post_listing = soup.find_all('li', { 'class': 'result-row'})
+	post_listing = soup.find_all({ 'class': 'products osh_gallery-no_gutter'})
 
 	fina_postings = []
 	for post in post_listing:
-		post_title = post.find(class_ = 'result-title').text
+		post_title = post.find(class_ = 'text text-title -align-left -mbxs -ellipsis-2').text
 		post_url = post.find('a').get('href')
-		if post.find(class_='result-price'):
-			post_price = post.find(class_='result-price').text
+		if post.find(class_='text text-body-1 color-default -mrxs -inline-block js-actual-price'):
+			friday_pirce = post.find(class_='text text-body-1 color-default -mrxs -inline-block js-actual-price').text
+			post_price = post.find(class_='text text-caption-default-800 -strike -inline-block -mrxs js-old-price').text
 		else:
 			post_price = 'N/A'
 
-		if post.find(class_='result-image').get('data-ids'):
-			post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
-			post_image_url = BASE_IMAGE_URL.format(post_image_id)
+		if post.find(class_='lazy card-sku_list--image -block-center -loaded')
+			post_image_id = 
+			post_image_url = post.find(class_='lazy card-sku_list--image -block-center -loaded').text
 			print(post_image_url)
 		else:
 			post_image_url = 'https://craigslist.org/images/peace.jpg'
