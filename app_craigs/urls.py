0ada6eedb0e8c429229d7837dd6bcd4c391d1eaa
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2019-12-17 18:44:16 +0300

    Adding new functionality

--- app_craigs/urls.py
+++ app_craigs/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home_view, new_search_view
+from .views import home_view, new_search_view, at
 
 urlpatterns = [
 	path('', home_view, name = 'home_view'),
 	path('new_search', new_search_view, name = 'new_search_view' ),
-	path('at', home_view, name = 'at'),
+	path('at', at, name = 'at'),
 ]
